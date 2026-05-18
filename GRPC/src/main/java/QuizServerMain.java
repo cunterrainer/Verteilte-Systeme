c8affab7a3b8bb1cf4f89290400308c5a486914a
ChristianUnterrainer <christian.unterrainer@vector.com>
2026-05-18 18:01:12 +0200

    Remove interrupt handler

--- GRPC/src/main/java/QuizServerMain.java
+++ GRPC/src/main/java/QuizServerMain.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class QuizServerMain {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         int port = args.length > 0 ? Integer.parseInt(args[0]) : 5555;
         int roundSeconds = args.length > 1 ? Integer.parseInt(args[1]) : 30;
         QuizServerState state = new QuizServerState(new QuestionBank());
@@ -15,21 +15,8 @@ public class QuizServerMain {
                 .addService(service)
                 .build();
 
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            server.shutdown();
-            try {
-                server.awaitTermination(5, TimeUnit.SECONDS);
-            } catch (InterruptedException interruptedException) {
-                Thread.currentThread().interrupt();
-            }
-        }, "grpc-shutdown-hook"));
-
-        try {
-            server.start();
-            System.out.println("QuizServer gRPC listening on port " + port);
-            server.awaitTermination();
-        } catch (InterruptedException interruptedException) {
-            Thread.currentThread().interrupt();
-        }
+        server.start();
+        System.out.println("QuizServer listening on port " + port);
+        server.awaitTermination();
     }
 }
